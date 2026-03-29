82f7db14e1e785af7cda0baeac48a038f58d13b5
pmorettobr <pmorettobr@gmail.com>
2026-03-29 15:29:38 -0300

    Add files via upload

--- report/repair_machine_report.py
+++ report/repair_machine_report.py
@@ -6,6 +6,7 @@ class RepairMachineReport(models.AbstractModel):
     _description = 'Relatório Programação por Máquina'
 
     def _get_report_values(self, docids, data=None):
+        from datetime import date, datetime
         wizard = self.env['repair.machine.report.wizard'].browse(docids)
         processes = wizard._get_processes()
 
@@ -18,10 +19,22 @@ class RepairMachineReport(models.AbstractModel):
                 machines[key] = {'machine': proc.machine_id, 'label': label, 'processes': []}
             machines[key]['processes'].append(proc)
 
+        # Garante que report_date é um objeto date com strftime
+        raw_date = wizard.report_date
+        if isinstance(raw_date, str):
+            try:
+                report_date = datetime.strptime(raw_date, '%Y-%m-%d').date()
+            except Exception:
+                report_date = date.today()
+        elif raw_date:
+            report_date = raw_date
+        else:
+            report_date = date.today()
+
         return {
             'doc_ids': docids,
             'doc_model': 'repair.machine.report.wizard',
             'docs': wizard,
             'machines': list(machines.values()),
-            'report_date': wizard.report_date,
+            'report_date': report_date,
         }
